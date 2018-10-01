318d8083b0042ac786092d46a1827e9bb1972253
Tashus <alheadbme@gmail.com>
2018-09-30 22:23:59 -0500

    Initial commit.

---
+++ InputManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+	string hAxis = "Horizontal";
+	string vAxis = "Vertical";
+	
+	KeyCode greenButton = KeyCode.JoystickButton0;
+	KeyCode redButton = KeyCode.JoystickButton1;
+	KeyCode blueButton = KeyCode.JoystickButton2;
+	KeyCode yellowButton = KeyCode.JoystickButton3;
+	KeyCode jumpButton = KeyCode.JoystickButton5;
+	
+	Vector3 inputDir = new Vector3();
+	
+	PlayerCharacter pc;
+
+	// Use this for initialization
+	void Start () {
+		pc = GetComponent<PlayerCharacter> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveHorizontal = Input.GetAxis (hAxis);
+		float moveVertical = Input.GetAxis (vAxis);
+		
+		inputDir.Set (moveHorizontal, moveVertical, 0f);
+		
+		pc.SetDirection (inputDir);
+		
+		/*if (Input.GetButton(greenButton)) {
+			pc.Cast(0);
+		}
+		if (Input.GetButton(redButton)) {
+			pc.Cast(1);
+		}
+		if (Input.GetButton(blueButton)) {
+			pc.Cast(2);
+		}
+		if (Input.GetButton(yellowButton)) {
+			pc.Cast(3);
+		}*/
+		
+
+		if (Input.GetKey(KeyCode.JoystickButton5)) {
+			pc.Jump();
+		}
+		
+	}
+}
---
+++ PlayerCharacter.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour {
+
+	CharacterController charCon;
+	Vector3 moveDir;
+	float maxSpeed = 250f;
+	
+	float jumpSpeed = 8f;
+	float gravity = 20f;	
+	
+	List<Ability> abilities = new List<Ability>();
+	
+	GameObject room;
+
+	// Use this for initialization
+	void Start () {
+		charCon = GetComponent<CharacterController> ();
+		for (int i = 0; i < 4; i++) {
+			abilities.Add(new Ability());
+		}
+		
+		room = GameObject.Find("VRWorld");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		charCon.SimpleMove (moveDir * maxSpeed * Time.fixedDeltaTime);
+		room.transform.Translate((transform.position.x - room.transform.position.x) * Vector3.right);
+	}
+
+	public void SetDirection ( Vector3 moveInput) {
+		moveDir = moveInput;
+	}
+	
+	public void Jump () {
+			if (charCon.isGrounded) {
+			charCon.SimpleMove (Vector3.up * jumpSpeed);
+			Debug.Log("Jump");
+		}
+	}
+	
+	public void Cast(int slot){
+		abilities[slot].Cast();
+	}
+
+}
